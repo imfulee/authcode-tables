a9204edb9504f5497fa9c4116cd3b2cb942520fe
imfulee <imfulee@gmail.com>
2022-06-07 17:22:06 +0800

    update styles and port

--- src/components/Lists.svelte
+++ src/components/Lists.svelte
@@ -1,7 +1,8 @@
 <script>
 	import { Table, Button, Icon } from 'sveltestrap';
 	import { authcodes, delete_authcode } from '../stores/authcode_store.js';
-	import { search_term, searching } from '../stores/search_store.js';
+	import { search_term } from '../stores/search_store.js';
+
 	import ListElem from './ListElem.svelte';
 	let searched_authcodes;
 	$: searched_authcodes = $authcodes.filter((code) => code.product_name == $search_term || code.system_num == $search_term || code.auth_num == $search_term || code.company_name == $search_term || code.auth_start_date == $search_term || code.auth_end_date == $search_term || code.system_num == $search_term || code.case_name == $search_term );
